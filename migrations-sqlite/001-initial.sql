4a5c9202a18823555f0bc9f14fb738245c09da92
ojnode <ojinnakabu95@gmail.com>
2025-03-10 22:50:17 +0000

    change primary key to serial

--- migrations-sqlite/001-initial.sql
+++ migrations-sqlite/001-initial.sql
@@ -1,14 +1,13 @@
 -- Up
 
 CREATE TABLE Runners (
-    id CHAR(36) PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     runnerName TEXT NOT NULL,
     time DATETIME
 );
 
-INSERT INTO Runners (id, runnername, time) VALUES
-( 'xhhjsjsjjj',
-    'John Paul',
+INSERT INTO Runners (runnername, time) VALUES
+(   'John Paul',
     datetime('now'))
 
 -- Down
