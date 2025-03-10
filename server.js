983b93bf2c435e5053872771ac3187281f7f5bf7
ojnode <ojinnakabu95@gmail.com>
2025-03-10 22:52:18 +0000

    create APIs for database querying

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 import 'dotenv/config';
 import express from 'express';
+import * as db from './public/database.js';
+
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-const macIp = process.env.macIp;
+async function saveRunner(req, res) {
+    const { runnerName } = req.body;
+    const runner = await db.addRunners(runnerName);
 
-app.listen(port, macIp, () => {
+    res.json(runner);
+}
+
+async function getRunners(req, res) {
+    res.json(await db.listRunners());
+}
+
+async function getRunner(req, res) {
+    res.json(await db.findRunner(req.params.id));
+}
+
+async function delRunner(req, res) {
+    const runner = await db.delRunner(req.params.id);
+    
+    res.json(runner);
+}
+
+async function putRunner(req, res) {
+    const { runnerName } = req.body;
+    const runner = await db.editRunner(req.params.id, runnerName);
+
+    res.json(runner);
+}
+
+app.post('/runner', express.json(), saveRunner);
+app.put('/runner/:id', express.json(), putRunner);
+app.delete('/runner/:id', delRunner);
+app.get('/runner', getRunners);
+app.get('/runner/:id', getRunner);
+
+app.listen(port, '0.0.0.0', () => {
     console.log(`listening on port ${port}`);
 });
