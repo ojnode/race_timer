da494e8c0d3d9c4315ec63b054ddd26a407815d5
ojnode <ojinnakabu95@gmail.com>
2025-03-14 16:31:47 +0000

    utils again, I dont know what i am doing

--- public/utils.js
+++ public/utils.js
@@ -1,18 +1,11 @@
-// create buttons
-export function addButton(parentId, textContent) {
-    const parentElem = document.querySelector(`#${parentId}`);
-    const button = document.createElement('button');
-    button.textContent = textContent;
-    parentElem.append(button);
-    return button;
-}
-
-//add touch effect
-export function touchEffect(elem) {
-    elem.addEventListener('touchstart', () => {
+//addeventlisteners
+export function setupButtons(elem, func) {
+    elem.addEventListener("touchstart", () => {
         elem.classList.toggle('active');
     });
-    elem.addEventListener('touchend', () => {
+    elem.addEventListener("touchend", () => {
         elem.classList.toggle('active');
+        console.log('hello hi');
+        func();
     });
 }
